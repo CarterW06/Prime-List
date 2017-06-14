666438bf3f81d392be6d75613202e7c0c8f376aa
benanddana92@gmail.com <benanddana92@gmail.com>
2017-06-14 12:15:20 -0700

    Reverted PrimeTester settings

--- src/PrimeTester.java
+++ src/PrimeTester.java
@@ -1,15 +1,12 @@
 
 public class PrimeTester {
-	// Uses Wilson's theorem
 	protected static boolean isPrime(int i) {
-		int product = 1;
-		for (int j = 1; j < i; j++) {
-			product *= j;
+		for (int j = i - 1; j > 1; j--) {
+			if (i % j == 0) {
+				return false;
+			}
 		}
-		if ((product + 1) % i == 0) {
-			return true;
-		}
-		return false;
+		return true;
 	}
 
 }
